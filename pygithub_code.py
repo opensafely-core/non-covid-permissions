4b03423ece2fddb8ae28b667c72ca52edd558462
Providence-o <97491889+Providence-o@users.noreply.github.com>
2026-02-11 19:09:09 +0000

    Rename 'toy1.py' to 'pygithub_code.py'

--- pygithub_code.py
+++ pygithub_code.py
@@ -1,3 +1,5 @@
+# This is a proof of concept spike. It is complex and time-consuming to run so will be kept for reference
+
 from github import Github, Auth
 import os
 from dotenv import load_dotenv
@@ -18,11 +20,11 @@ auth = Auth.Token(API_TOKEN)
 
 g = Github(auth=auth)
 
-# checks within the last 9 months
+# Checks within the last 9 months
 start_time_naive = datetime.now() - timedelta(days=270)  # no timezone info
-start_time = start_time_naive.replace(
-    tzinfo=timezone.utc
-)  # converts to timezone aware, the format in which git commit dates are stored
+
+# Convert to timezone aware, the format in which git commit dates are stored
+start_time = start_time_naive.replace(tzinfo=timezone.utc)
 
 end_time_naive = datetime.now()
 end_time = end_time_naive.replace(tzinfo=timezone.utc)
@@ -33,6 +35,7 @@ def get_file_info(repo, contents):
     while contents:
         # breakpoint()
 
+        # tmp: same as a one ContentFile in contentFile.py
         repo_content = contents.pop(0)
 
         if repo_content.path.endswith(".py"):
@@ -42,14 +45,21 @@ def get_file_info(repo, contents):
             # print(file_content.decoded_content.decode("utf-8"))  # decodes and converts to string # TODO: use this code
             commit = repo.get_commits(path=repo_content.path)
             # breakpoint()
+
+            # TODO: Some of the latest commits were not made by the main researcher. This was usually by a developer. Think about how to handle this case. Will the top repo contributor be a better estimate.
+            # See opensafely/post-covid-renal as example
+            # TODO: add link to the docs that reference this code
             latest_commit = commit[0]
+            print(dir(latest_commit))  ## to delete
             time_interval = start_time < latest_commit.commit.author.date <= end_time
 
+            # Skips to the next iteration if the date of the last commit is not within the time
             if not time_interval:
                 continue
             # if time_interval:
             # TODO: ast parsing applied here
 
+            # The 'decoded_content' method returns a bytes string (ast cannot work with this data type). Calling 'decode' on this method converts it to a string
             data = repo_content.decoded_content.decode("utf-8")
             ast_tree = ast.parse(data)
 
